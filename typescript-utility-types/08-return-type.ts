db8767ed8d2278b949a9323054123dc88a6afeee
midudev <miduga@gmail.com>
2025-09-18 17:46:00 +0200

    Add examples for ReturnType<T> utility type in 08-return-type.ts

--- typescript-utility-types/08-return-type.ts
+++ typescript-utility-types/08-return-type.ts
@@ -1,2 +1,20 @@
 // --- 08: ReturnType<T> ---
-// Obtiene el tipo de retorno de una función.
+// Obtiene el tipo de retorno de una función
+
+const createAuthResponse = (success: boolean) => {
+  if (success) {
+    const token = crypto.randomUUID()
+    return { status: 200, token } as const
+  }
+
+  return { status: 401, error: "Unauthorized" } as const
+}
+
+const response = createAuthResponse(true)
+
+type AuthResponse = ReturnType<typeof createAuthResponse>
+
+const authResponse: AuthResponse = {
+  status: 401,
+  error: "Unauthorized"
+}
